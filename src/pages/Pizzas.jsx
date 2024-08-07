ad06ba6cd37c35a8dd39705ede74e5a03333a20d
RafaelMarquesO1 <rafaelmarquesm.0209@gmail.com>
2024-08-07 11:45:25 -0300

    Imagens e etc...

---
+++ src/pages/Pizzas.jsx
@@ -0,0 +1,44 @@
+import margherita from "../assets/images/PizzaDeMargherita.png"
+import calabresa from "../assets/images/PizzaCalabresa.png"
+import vegetariana from "../assets/images/PizzaVegetarian.png"
+import havaiana from "../assets/images/PizzaHavaiana.png"
+import bacon from "../assets/images/PizzaHavaiana.png"
+
+const Pizzas = [
+    {
+        "id": 1,
+        "nome": "Margherita",
+        "descricao": "Molho de Tomate e Margherita",
+        "imagem": margherita
+    },
+
+    {
+        "id": 2,
+        "nome": "Calabresa",
+        "descricao": "Molho de tomate com Calabresa",
+        "imagem": calabresa
+    },
+
+    {
+        "id": 3,
+        "nome": "Vegetariana",
+        "descricao": "Legumes e Tomate...",
+        "imagem": vegetariana
+    },
+
+    {
+        "id": 4,
+        "nome": "Havaiana",
+        "descricao": "Queijo e legumes...",
+        "imagem": havaiana
+    },
+
+    {
+        "id": 5,
+        "nome": "Bacon",
+        "descricao": "Molho de Tomate, Queijo e Bacon",
+        "imagem": bacon
+    }
+]
+
+export default Pizzas;
